1b0f65bc70be612dc39a53d4654d5e13eefe6b69
Laurent Ghigonis <ooookiwi@gmail.com>
2022-10-07 14:31:41 +0200

    data_gathering: make remote more configurable and take ssh alias as node name

--- data_gathering/remote_generate_data.sh
+++ data_gathering/remote_generate_data.sh
@@ -3,6 +3,7 @@
 trace() { echo "# $*"; "$@"; }
 D="$(dirname $0)"
 SSH_OPTS=${SSH_OPTS:-""}
+TMP_DIR=${TMP_DIR:-"/tmp"}
 
 set -e
 
@@ -15,10 +16,11 @@ output_dir="$1" && shift
 now=$(date +%Y%m%d_%H%M%S)
 echo "$now $*" >> "$output_dir/remote_generate_data_${now}.log"
 echo "SSH_OPTS='$SSH_OPTS'"
+echo "TMP_DIR='$TMP_DIR'"
 
 for remote_host in "$@"; do
 	sshalias=$(echo $remote_host |cut -d: -f1)
-	ip=$(ssh -G $sshalias |egrep -i "^hostname" |cut -d' ' -f2)
+	ip=$sshalias
 	k8s=0
 	ssh_pre=""
 	while read opt; do
@@ -28,15 +30,15 @@ for remote_host in "$@"; do
 		esac
 	done < <(echo $remote_host |cut -d: -f2- |tr ':' '\n')
 	echo "[+] gathering data from $sshalias ($ip) ssh_pre='$ssh_pre' k8s=$k8s"
-	trace $ssh_pre ssh $SSH_OPTS $sshalias "mkdir -p /tmp/netmap/data"
-	trace $ssh_pre scp $SSH_OPTS $D/generate_data.sh $sshalias:/tmp/netmap/
-	trace $ssh_pre ssh $SSH_OPTS $sshalias "/tmp/netmap/generate_data.sh $gendata_opts /tmp/netmap/data $ip"
+	trace $ssh_pre ssh $SSH_OPTS $sshalias "mkdir -p $TMP_DIR/netmap/data"
+	trace $ssh_pre scp $SSH_OPTS $D/generate_data.sh $sshalias:$TMP_DIR/netmap/
+	trace $ssh_pre ssh -t $SSH_OPTS $sshalias "TCPDUMP_TIME=$TCPDUMP_TIME TCPDUMP_PACKETS=$TCPDUMP_PACKETS $TMP_DIR/netmap/generate_data.sh $gendata_opts $TMP_DIR/netmap/data $ip"
 	if [ $k8s -eq 1 ]; then
-		trace $ssh_pre scp $SSH_OPTS $D/generate_data_k8s.sh $sshalias:/tmp/netmap/
-		trace $ssh_pre ssh $SSH_OPTS $sshalias "SSH_NODES_CMD="$SSH_NODES_CMD" /tmp/netmap/generate_data_k8s.sh $gendata_opts /tmp/netmap/data $ip"
+		trace $ssh_pre scp $SSH_OPTS $D/generate_data_k8s.sh $sshalias:$TMP_DIR/netmap/
+		trace $ssh_pre ssh -t $SSH_OPTS $sshalias "SSH_NODES_CMD="$SSH_NODES_CMD" TCPDUMP_TIME=$TCPDUMP_TIME TCPDUMP_PACKETS=$TCPDUMP_PACKETS $TMP_DIR/netmap/generate_data_k8s.sh $gendata_opts $TMP_DIR/netmap/data $ip"
 	fi
-	trace $ssh_pre rsync -e "ssh $SSH_OPTS" -avPz $sshalias:"/tmp/netmap/data/" $output_dir || trace $ssh_pre scp $SSH_OPTS -r $sshalias:"/tmp/netmap/data/*" $output_dir
-	trace $ssh_pre ssh $SSH_OPTS $sshalias "rm -rf /tmp/netmap"
+	trace $ssh_pre rsync -e "ssh $SSH_OPTS" -avPz $sshalias:"$TMP_DIR/netmap/data/" $output_dir || trace $ssh_pre scp $SSH_OPTS -r $sshalias:"$TMP_DIR/netmap/data/*" $output_dir
+	trace $ssh_pre ssh $SSH_OPTS $sshalias "rm -rf $TMP_DIR/netmap"
 done
 
 files_count=$(ls -1 $output_dir |wc -l)
