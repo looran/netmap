2a42d24a420dff1c94ef1d64aaeb9ee626ca3706
Laurent Ghigonis <ooookiwi@gmail.com>
2022-10-07 14:33:51 +0200

    web: cleanup start script and more explicit help

--- webserver.sh
+++ webserver.sh
@@ -1,24 +1,34 @@
 #!/bin/sh
 
+usageexit() {
+	cat <<-_EOF
+usage: $0 [-C] [-d] [-d] [-a <anonymize_hex_salt>] <input_data_directory> [<flask options>]
+   -C : disable cache
+   -d : activate debug output, twice disables multithreading
+   -a <salt> : anonymise output network data
+_EOF
+	exit 1
+}
+trace() { echo "$ $*" >&2; "$@"; }
+
 DIR="$(dirname $0)"
 
 set -e
 
-[ $# -lt 1 -o "$1" = "-h" ] && echo "usage: $0 [-C] [-d] [-a <anonymize_hex_salt>] <input_data_directory> [<flask options>]" && exit 1
-
-set -x
+[ $# -lt 1 -o "$1" = "-h" ] && usageexit
 
 [ "$1" = "-C" ] && NETMAP_DISABLE_CACHE=1 && shift
-[ "$1" = "-d" ] && FLASK_DEBUG=1 && shift
+[ "$1" = "-d" ] && FLASK_DEBUG=1 && NETMAP_DEBUG=1 && shift
+[ "$1" = "-d" ] && NETMAP_DEBUG=2 && shift
 [ "$1" = "-a" ] && NETMAP_ANONYMIZE_HEX_SALT=$2 && shift && shift
 input_data_directory="$1" && shift
 
-export FLASK_APP=netmap_flask.py
-export FLASK_DEBUG=${FLASK_DEBUG:-0}
-export NETMAP_INPUT_DATA_DIRECTORY="$(realpath $input_data_directory)"
-export NETMAP_DISABLE_CACHE="${NETMAP_DISABLE_CACHE:-0}"
-export NETMAP_DEBUG=${NETMAP_DEBUG:-$FLASK_DEBUG}
-export NETMAP_ANONYMIZE_HEX_SALT=${NETMAP_ANONYMIZE_HEX_SALT:-""}
+trace export FLASK_APP=netmap_flask.py
+trace export FLASK_DEBUG=${FLASK_DEBUG:-0}
+trace export NETMAP_INPUT_DATA_DIRECTORY="$(realpath $input_data_directory)"
+trace export NETMAP_DISABLE_CACHE="${NETMAP_DISABLE_CACHE:-0}"
+trace export NETMAP_DEBUG=${NETMAP_DEBUG:-$FLASK_DEBUG}
+trace export NETMAP_ANONYMIZE_HEX_SALT=${NETMAP_ANONYMIZE_HEX_SALT:-""}
 
-cd $DIR/flask
-python3 -m flask run $@
+trace cd $DIR/flask
+trace python3 -m flask run $@
